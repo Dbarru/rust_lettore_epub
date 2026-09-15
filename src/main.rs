181ed3dc599451a93982b0e5165bd71b42ad3526
Dbarru <darioburragato@gmail.com>
2026-09-15 15:02:19 +0200

    otteniamo l'ordine di lettura dei file

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ use std::collections::HashMap;
 
 
 fn main() { 
-    let file = File::open(r"/home/barru/Scrivania/progetti/kobo-book-downloader-master/tutti_libri/Adharanand Finn - The Rise of the Ultra Runners.epub")
+    let file = File::open(r"C:\Users\Burragato.MASCIA\Desktop\progetti\rust\epub\Vento e Verità Brandon Sanderson z-library.sk, 1lib.sk, z-lib.sk.epub")
         .expect("Failed to open file");
 
     let mut zip = zip::ZipArchive::new(file).expect("...");
     let content = read_container_xml(&mut zip).expect("non  ho trovato nulla");
     let indice = read_content_xml(&mut zip, &content).expect("non ho trovato i contenuti");
-    let parse = roxmltree::Document::parse(&indice);
-    println!("{:?}", parse);
+    let parse1 = roxmltree::Document::parse(&indice).unwrap();
+    let manifest = extract_manifest(&parse1);
+    let spine = extract_spine(&parse1);
+    let ordine_libro1 = build_reading_order(&spine, &manifest);
+    println!("{:?}",ordine_libro1 )
 }
 
 
@@ -37,15 +40,35 @@ fn read_content_xml<R:Read + Seek>(zip:&mut zip::ZipArchive<R>, path : &String) 
 }
 
 fn extract_manifest(contenuto:&roxmltree::Document)-> HashMap<String, String>{
-    todo!()
+    let mut hashmap_indice = HashMap::new();
+    for node in contenuto.descendants(){
+        if node.has_tag_name("item"){
+            let id:&str = node.attribute("id").unwrap();
+            let href:&str = node.attribute("href").unwrap();
+            hashmap_indice.insert(
+                 id.to_string(),
+                href.to_string()
+            );
+        }  
+    }; 
+    hashmap_indice
 }
-// fn list_zip_contents(reader: impl Read + Seek) -> zip::result::ZipResult<()> {
-//     let mut zip = zip::ZipArchive::new(reader)?;
 
-//     for i in 0..zip.len() {
-//         let file = zip.by_index(i)?;
-//         println!("Filename: {}", file.name());
-//     }
+fn extract_spine(contenuto:&roxmltree::Document)-> Vec<String>{
+    let mut spine_indice = Vec::new();
+    for node in contenuto.descendants(){
+        if node.has_tag_name("itemref"){
+            let idref:&str = node.attribute("idref").unwrap();
+            spine_indice.push(idref.to_string());
+        }  
+    }; 
+    spine_indice
+}
 
-//     Ok(())
-// }
+fn build_reading_order(vettore: &Vec<String>, mappa : &HashMap<String, String>) -> Vec<String>{
+    let mut ordine_libro: Vec<String> = Vec::new();
+    for ind in vettore {
+        ordine_libro.push(mappa.get(ind).unwrap().to_string());
+    }
+    ordine_libro
+}
